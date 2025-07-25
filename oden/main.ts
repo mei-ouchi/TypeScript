5de02a4a4a69ee539dc2c3fedb3a362df52b4c31
EXCITE-2108-02\mei ouchi <mei.ouchi@excite-software.net>
2025-07-25 14:32:52 +0900

    8章ChatGPT APIで料理レシピを作成

--- oden/main.ts
+++ oden/main.ts
@@ -1,8 +1,25 @@
-async function headler(){
-  const html = await Deno.readTextFile("./index.html");
-  const response = new Response(html, {
+import OpenAI from "https://deno.land/x/openai@v4.62.1/mod.ts";
+
+const openai = new OpenAI({
+  apiKey: Deno.env.get("OPENAI_API_KEY"),
+});
+
+const RECIPE_ROUTE = new URLPattern({pathname: "/:name"});
+
+async function headler(req: Request){
+  const match = RECIPE_ROUTE.exec(req.url);
+  const name: string = match?.pathname.groups.name ?? "おでん";
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {role: "system", content: "レシピ作成アシスタント"},
+      {role: "user", content: name},
+    ],
+    model: "gpt-4o-mini",
+  });
+  const content = completion.choices[0].message.content;
+  const response = new Response(content, {
     headers: {
-        "content-type":"text/html;charset=utf-8",
+      "content-type": "text/plain;charset=utf-8",
     },
   });
   return response;
